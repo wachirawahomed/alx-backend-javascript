e9b9fcd25e907f9aee32141efef7171c32b63148
wachirawahomed <wachirawahomed@gmail.com>
2025-01-08 21:59:01 +0000

    100-createIteratorObject.js v.0.2

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,20 +1,20 @@
 export default function createIteratorObject(report) {
-  const employees = [];
-  for (const department of Object.values(report.allEmployees)) {
-    employees.push(...department);
-  }
-
-  let index = 0;
-
-  return {
-    next() {
-      if (index < employees.length) {
-        return { value: employees[index++], done: false }; // Increment handled here
-      }
-      return { done: true };
-    },
-    [Symbol.iterator]() {
-      return this; // Allow use in a for...of loop
-    },
-  };
+	const employees = [];
+	for (const department of Object.values(report.allEmployees)){
+		employees.push(...department);
+	}
+	
+	let index = 0;
+	return {
+		next() {
+			if (index < employees.length) {
+				return { value: employees[index++], done: false }; // Increment handled here
+			}
+			
+			return { done: true };
+		},
+		[Symbol.iterator]() {
+			return this; // Allow use in a for...of loop
+		},
+	};
 }
