3443359d581499df347d82c1c95d46dd793b0743
wachirawahomed <wachirawahomed@gmail.com>
2025-01-10 20:23:12 +0000

    2-then.js revised 0.1

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,13 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      return new Error();
     });
 }
+
+export default handleResponseFromAPI;
